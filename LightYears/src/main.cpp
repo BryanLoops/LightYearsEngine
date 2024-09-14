697a9799941149032c5f8d2debf94e5edbda6deb
Bryan Lopes <bryan.lopes89@outlook.com>
2024-09-14 11:05:56 -0300

    included dlls into cmake build

--- LightYears/src/main.cpp
+++ LightYears/src/main.cpp
@@ -3,5 +3,19 @@
 
 int main()
 {
+	sf::RenderWindow renderWindow{ sf::VideoMode(100, 100), "MyWindow" };
+
+	while (renderWindow.isOpen())
+	{
+		sf::Event windowEvent;
+		while (renderWindow.pollEvent(windowEvent))
+		{
+			if (windowEvent.type == sf::Event::EventType::Closed)
+			{
+				renderWindow.close();
+			}
+		}
+	}
+
 	std::cout << "hello world!" << std::endl;
 }

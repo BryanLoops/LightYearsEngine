3a895b955861237aecb904bb02405c4acd6bda00
Bryan Lopes <bryan.lopes89@outlook.com>
2024-09-14 22:51:51 -0300

    added log+separated engine and game

--- LightYearsEngine/src/framework/Application.cpp
+++ LightYearsEngine/src/framework/Application.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include "framework/Application.h"
+#include "framework/Core.h"
 
 namespace ly
 {
 	Application::Application()
-		: mWindow{ sf::VideoMode(480, 640), "Light Years" },
+		: mWindow{ sf::VideoMode(480, 720), "Light Years" },
 		mTargetFrameRate{60.f},
 		mTickClock{}
 	{
@@ -30,17 +31,35 @@ namespace ly
 			while (accumulatedTime > targetDeltaTime)
 			{
 				accumulatedTime -= targetDeltaTime;
-				Tick(targetDeltaTime);
-				Render();
+				TickInternal(targetDeltaTime);
+				RenderInternal();
 			}
-			std::cout << "ticking at framerate: " << 1.f / frameDeltaTime << std::endl;
+			LOG("ticking at framerate: %f", 1.f / frameDeltaTime);
 		}
 	}
-	void Application::Tick(float deltaTime)
+	void Application::TickInternal(float deltaTime)
 	{
-		
+		Tick(deltaTime);
+	}
+	void Application::RenderInternal()
+	{
+		mWindow.clear();
+
+		Render();
+
+		mWindow.display();
 	}
 	void Application::Render()
 	{
+		sf::RectangleShape rect{ sf::Vector2f{100, 100} };
+		rect.setFillColor(sf::Color::Green);
+		rect.setOrigin(50, 50);
+		rect.setPosition(mWindow.getSize().x / 2.f, mWindow.getSize().y / 2.f);
+
+		mWindow.draw(rect);
+	}
+	void Application::Tick(float deltaTime)
+	{
+
 	}
 }
